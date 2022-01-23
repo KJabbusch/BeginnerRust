91f9e328f9ce3bb63c9b21b20278250a2bb3d3a1
Kristel Jabbusch <kaj12@my.fsu.edu>
2022-01-22 16:54:31 -0800

    mid test push

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -47,6 +47,15 @@ mod tests {
         assert_eq!(4, add_two(2));
     }
 
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -61,4 +70,8 @@ impl Rectangle {
 
 pub fn add_two(a: i32) -> i32 {
     a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
 }
