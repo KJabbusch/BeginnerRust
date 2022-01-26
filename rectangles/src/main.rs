0996f4c9619483d6d6bab12590cf7221f60dfcad
Kristel Jabbusch <kaj12@my.fsu.edu>
2022-01-25 18:01:04 -0800

    creating a method for struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -46,37 +46,63 @@
 //     );
 // }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
-
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
 
 // REFACTORED AGAIN AGAIN lol
 
+// #[derive(Debug)]
+// // above will print out debugging information
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 3;
+//     let rect1 = Rectangle {
+//         // dbg is a macro takes ownership of an expression, prints the file and line num of where it was called along w/ the resulting value of that expression
+//         // dbg returns ownership of the value 
+//         // dbg prints to the standrd error console stream (stderr)
+//         // whereas println! prints to the standard output stream (stdout)
+//         width: dbg!(30*scale),
+//         height: dbg!(50*scale),
+//     };
+//     println!(
+//         "rect1 is {:?}",
+//         rect1
+//     );
+//     println!(
+//         "The area of the rectange is {} square pixels.",
+//         // below we pass a reference to the struct
+//         area(&rect1)
+//     );
+// }
+
+// REFACTORED AGAIN AGAIN AGAIN!!!!!
+
 #[derive(Debug)]
-// above will print out debugging information
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 3;
     let rect1 = Rectangle {
-        // dbg is a macro takes ownership of an expression, prints the file and line num of where it was called along w/ the resulting value of that expression
-        // dbg returns ownership of the value 
-        // dbg prints to the standrd error console stream (stderr)
-        // whereas println! prints to the standard output stream (stdout)
-        width: dbg!(30*scale),
-        height: dbg!(50*scale),
+        width: 30,
+        height: 50,
     };
+
     println!(
-        "rect1 is {:?}",
-        rect1
-    );
-    println!(
-        "The area of the rectange is {} square pixels.",
-        // below we pass a reference to the struct
-        area(&rect1)
+        "The area of rectangle is {} square pixels.",
+        rect1.area()
     );
 }
+
